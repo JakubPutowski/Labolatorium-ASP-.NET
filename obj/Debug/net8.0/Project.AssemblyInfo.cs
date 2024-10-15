1871dd04c89a0d8c51a5eb8d5a042d552d5c34fb
jakub.putowski <jakub.putowski@microsoft.wsei.edu.pl>
2024-10-15 11:55:30 +0200

    lab1

--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0e98157242f74884469e198d3a92db17bbd59e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac5b441f2e9f82dd814fe08ec4caca9fb1548f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
